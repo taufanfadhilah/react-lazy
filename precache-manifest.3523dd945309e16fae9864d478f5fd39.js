b56d4ad63907b8ec461f3185ce5a33a6f6faad20
Taufan Fadhilah Iskandar <taufanfadhilahiskandar@gmail.com>
2019-06-03 15:15:45 +0700

    Updates

---
+++ precache-manifest.3523dd945309e16fae9864d478f5fd39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "179ad4f4945125c299876d3968fc5b2b",
+    "url": "/react-lazy/index.html"
+  },
+  {
+    "revision": "e1a411f1cfd858ca0977",
+    "url": "/react-lazy/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "67067dcfbccab3e0f8b9",
+    "url": "/react-lazy/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e1a411f1cfd858ca0977",
+    "url": "/react-lazy/static/js/2.93ed5dd5.chunk.js"
+  },
+  {
+    "revision": "4486a9a9cb91f825a06c",
+    "url": "/react-lazy/static/js/3.b74ed60b.chunk.js"
+  },
+  {
+    "revision": "67067dcfbccab3e0f8b9",
+    "url": "/react-lazy/static/js/main.f852b4a3.chunk.js"
+  },
+  {
+    "revision": "7bcac6f1efe9d71a5a7f",
+    "url": "/react-lazy/static/js/runtime~main.b458ce9f.js"
+  }
+]);
